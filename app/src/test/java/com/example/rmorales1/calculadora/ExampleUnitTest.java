a17ab88467d56e41f1b3527b00edca6a4a91137f
EnriquePallares <eapallaress@gmail.com>
2018-08-23 19:50:40 -0500

    Calculadora testeada

--- app/src/test/java/com/example/rmorales1/calculadora/ExampleUnitTest.java
+++ app/src/test/java/com/example/rmorales1/calculadora/ExampleUnitTest.java
@@ -10,8 +10,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros(){
+        double a = 4, b = 6, c;
+        c = 10;
+        assertEquals(c, Metodos.sumar(a, b), 0);
+    }
+
+    @Test
+    public void restar_dos_numeros(){
+        double a = 4, b = 6, c;
+        c = -2;
+        assertEquals(c, Metodos.restar(a, b), 0);
+    }
+
+    @Test
+    public void multiplicar_dos_numeros(){
+        double a = 4, b = 6, c;
+        c = 24;
+        assertEquals(c, Metodos.muliplicar(a, b), 0);
+    }
+
+    @Test
+    public void dividir_dos_numeros(){
+        double a = 4, b = 6, c;
+        c = a/b;
+        assertEquals(c, Metodos.dividir(a, b), 0);
     }
 }
